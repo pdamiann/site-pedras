5ad3501f5c086c67d062693448aac52146db98d8
Pedro Damian <159589006+pdamiann@users.noreply.github.com>
2025-04-10 13:00:01 -0300

    Add files via upload

---
+++ produtos.js
@@ -0,0 +1,20 @@
+const botoesFiltro = document.querySelectorAll('.btn');
+const cardsProduto = document.querySelectorAll('.produto-card');
+
+botoesFiltro.forEach(btn => {
+  btn.addEventListener('click', () => {
+    // Remove 'active' de todos e adiciona no atual
+    botoesFiltro.forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+
+    const filtro = btn.dataset.filtro;
+
+    cardsProduto.forEach(card => {
+      if (filtro === 'todos' || card.classList.contains(filtro)) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  });
+});
